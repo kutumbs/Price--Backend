8fb6b2117c4e13743c478bd642d059b59654caa4
jonathan <jonathan.123kutumbakana@gmail.com>
2024-05-20 10:54:19 +0300

    scraped,model.py by jonathan, mahdi

---
+++ server/model.py
@@ -0,0 +1,138 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy_serializer import SerializerMixin
+# from sqlalchemy import MetaData
+# from sqlalchemy.orm import validates
+
+# metadata = MetaData()
+# db = SQLAlchemy(metadata=metadata)
+
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(100), nullable=False)
+#     products = db.relationship('Product', backref='user', lazy=True, cascade="all, delete-orphan")
+
+#     @validates('password')
+#     def validate_password(self, key, password):
+#         if len(password) < 8:
+#             raise ValueError('Password must be more than 8 characters.')
+#         return password
+
+#     @validates('email')
+#     def validate_email(self, key, email):
+#         if not email.endswith("@gmail.com"):
+#             raise ValueError("Email is not valid. It should end with @gmail.com")
+#         return email
+
+# class Product(db.Model, SerializerMixin):
+#     __tablename__ = 'products'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.String, nullable=False)
+#     rating = db.Column(db.String)
+#     image_url = db.Column(db.String(100), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     stores = db.relationship('Store', secondary='productstores', backref=db.backref('products', lazy='dynamic'))
+
+# class Store(db.Model, SerializerMixin):
+#     __tablename__ = 'stores'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     rating = db.Column(db.Integer)
+#     product_stores = db.relationship('ProductStore', backref='stores', overlaps='products,productstores')
+
+# class ProductStore(db.Model, SerializerMixin):
+#     __tablename__ = 'productstores'
+#     id = db.Column(db.Integer, primary_key=True)
+#     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
+#     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+#     store = db.relationship('Store', backref='productstores', overlaps='productstores,products')
+#     product = db.relationship('Product', backref='stores', overlaps='stores,productstores')
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+import re
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pricehunter.db'  # Adjust as needed
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+metadata = MetaData()
+db = SQLAlchemy(app, metadata=metadata)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    products = db.relationship('Product', backref='user', lazy=True, cascade="all, delete-orphan")
+
+    @validates('password')
+    def validate_password(self, key, password):
+        if len(password) < 8:
+            raise ValueError('Password must be more than 8 characters.')
+        return password
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if not email.endswith("@gmail.com"):
+            raise ValueError("Email is not valid. It should end with @gmail.com")
+        return email
+
+    # @staticmethod
+    # def extract_numeric_price(price_str):
+    #     if not price_str:  # Check if the input string is None or empty
+    #         return None
+    #     # Remove any non-numeric characters, except for '.' (decimal separator)
+    #     numeric_price = re.sub(r'[^0-9.]', '', price_str)
+    #     if numeric_price:
+    #         return float(numeric_price)
+    #     return None
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.String, nullable=False)
+    rating = db.Column(db.String)
+    image_url = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    product_stores = db.relationship('ProductStore', backref='product', lazy='dynamic')
+
+class Store(db.Model, SerializerMixin):
+    __tablename__ = 'stores'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer)
+    product_stores = db.relationship('ProductStore', backref='store', lazy='dynamic')
+
+class ProductStore(db.Model, SerializerMixin):
+    __tablename__ = 'productstores'
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    url = db.Column(db.String(200))
+    rating = db.Column(db.String)
+    __table_args__ = (
+        db.UniqueConstraint('product_id', 'store_id', name='unique_product_store'),
+    )
+
+    @validates('price')
+    def validate_price(self, key, price):
+        # Ensure the price is properly formatted as a float
+        if isinstance(price, str):
+            price = User.extract_numeric_price(price)
+        if price is None:
+            raise ValueError('Invalid price format')
+        return price
+
+# Create the database and tables
+with app.app_context():
+    db.create_all()
---
+++ server/seed.py
@@ -0,0 +1,79 @@
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from model import db,Product
+from app import app
+
+rows = []
+
+# Kilimall Kenya
+kilimall_url = "https://www.kilimall.co.ke/search-result?keyword=wacth&source=search|enterSearch|wacth"
+
+# Alibaba
+alibaba_url = "https://www.alibaba.com/trade/search?spm=a2700.galleryofferlist.the-new-header_fy23_pc_search_bar.keydown__Enter&tab=all&SearchText=mac+laptop+computer"
+
+# Shopit
+shopit_url = "https://shopit.co.ke/?match=all&subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&q=phone&dispatch=products.search&security_hash=8eb5027cd936a66cb2edaa8c500b6e36"
+
+# Initialize Firefox WebDriver
+driver = webdriver.Firefox()
+
+# Initialize list to store scraped data
+scraped_data = []
+
+# Scraping Kilimall
+driver.get(kilimall_url)
+wait = WebDriverWait(driver, 10)
+kilimall_links = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div.product-item')))
+
+for link in kilimall_links:
+    scraped_data.append(link.text.split('\n'))
+
+# Scraping Alibaba
+driver.get(alibaba_url)
+wait = WebDriverWait(driver, 10)
+alibaba_links = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div.fy23-search-card')))
+
+for link in alibaba_links:
+    scraped_data.append(link.text.split('\n'))
+
+# Scraping Shopit
+driver.get(shopit_url)
+wait = WebDriverWait(driver, 10)
+shopit_links = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div.ut2-gl__body')))
+
+for link in shopit_links:
+    scraped_data.append(link.text.split('\n'))
+
+# Quit the WebDriver
+driver.quit()
+
+# Writing scraped data to CSV file
+
+with open("scraped_data.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+print(header)
+print(rows)
+
+with app.app_context():
+    
+    print('Deleting ...')
+    
+    Product.query.delete()
+
+    for item in rows:
+        product = Product(
+            name=item[0],
+            price=item[1],
+            rating=item[2],
+            image_url=item[3]
+        )
+        db.session.add(product)
+        db.session.commit()
+
+
